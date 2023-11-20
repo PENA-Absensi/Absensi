87cec2107a51fc13258bec4f77735b7097f87562
email <fikrinahkla2@gmail.com>
2023-11-20 07:49:00 -0500

    template

---
+++ resources/views/layout/sidebarAdmin.blade.php
@@ -0,0 +1,26 @@
+<div class="nk-sidebar">           
+    <div class="nk-nav-scroll">
+        <ul class="metismenu" id="menu">
+            <li>
+                <a class="" href="{{ route('home') }}" aria-expanded="false">
+                    <i class="icon-speedometer"></i><span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a class="" href="{{ route('status') }}" aria-expanded="false">
+                    <i class="icon-user"></i><span class="nav-text">Data Absensi</span>
+                </a>
+            </li>
+            <li>
+                <a class="" href="{{ route('kegiatan') }}" aria-expanded="false">
+                    <i class="icon-note"></i><span class="nav-text">Daftar Kegiatan</span>
+                </a>
+            </li>
+            <li>
+                <a class="" href="{{ route('manajemen') }}" aria-expanded="false">
+                    <i class="icon-check"></i><span class="nav-text">Manajemen User</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SesiController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Pages.dashboard');
+// Route::get('/', function () {
+//     return view('Pages.dashboard');
+// });
+
+// Route::middleware(['guest'])->group(function(){
+//     Route::get('/',[SesiController::class,'index'])->name('login');
+//     Route::post('/',[SesiController::class,'login']);
+// });
+
+// Route::middleware(['auth'])->group(function(){
+//     Route::get('/admin',[AdminController::class,'index']);
+//     Route::get('/logout',[SesiController::class,'logout']);
+// });
+
+// adminn
+// routes/web.php
+
+Route::prefix('/admin')->controller(AdminController::class)->group(function () {
+    Route::get('/Data-absensi', [AdminController::class, 'status'])->name('status');
+    Route::get('/home', [AdminController::class, 'home'])->name('home');
+    Route::get('/Data-kegiatan', [AdminController::class, 'kegiatan'])->name('kegiatan');
+    Route::get('/manajemen-user', [AdminController::class, 'manajemen'])->name('manajemen');
 });
+
+
+// akhir admin
+
+// users
+Route::prefix('/users')->controller(UsersController::class)->group(function () {
+    Route::get('/status-user', [UsersController::class, 'master'])->name('statusUser');
+    Route::get('/kegiatan-user', [UsersController::class, 'KegiatanUser'])->name('kegiatanUser');
+
+});
+Route::get('/users',function(){
+    return view('users.statusUser');
+});
+Route::get('/kegiatan-User',function(){
+    return view('users.kegiatanUser');
+});
+
+// akhir Users
+
+// Admin
+Route::get('/',function(){
+    return redirect('/admin');
+});
+Route::get('/admin',function(){
+    return view('admin.home');
+});
+// Akhir Admin
+
+
